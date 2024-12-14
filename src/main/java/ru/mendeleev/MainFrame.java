6faa6984b5b9ca786ac7c7cb5897e9910fc90b55
MaxMen5 <161643770+MaxMen5@users.noreply.github.com>
2024-12-14 16:15:01 +0300

    First commit.

---
+++ src/main/java/ru/mendeleev/MainFrame.java
@@ -0,0 +1,47 @@
+package ru.mendeleev;
+
+import org.springframework.stereotype.Component;
+import ru.mendeleev.utils.CommonUtils;
+
+import javax.annotation.PostConstruct;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+
+@Component
+public final class MainFrame extends JFrame {
+
+    private static final String TITLE = "Library";
+
+
+    public MainFrame() {
+
+    }
+
+    @PostConstruct
+    public void init() {
+        setTitle(TITLE);
+        createGUI();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(
+                        MainFrame.this,
+                        "Вы действительно хотите выйти?",
+                        "Выйти?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        setSize(CommonUtils.prepareWindowSizeWithShifts(0, 40));
+        setVisible(true);
+    }
+
+    private void createGUI() {
+
+    }
+
+}
