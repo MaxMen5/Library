a137d1977867b75e51a515cb160eb989c7505269
MaxMen5 <161643770+MaxMen5@users.noreply.github.com>
2024-12-16 22:17:51 +0300

    Server implementation.

--- library-server/src/main/java/ru/mendeleev/server/controller/LibraryServerServiceImpl.java
+++ library-server/src/main/java/ru/mendeleev/server/controller/LibraryServerServiceImpl.java
@@ -11,100 +11,118 @@ import ru.mendeleev.api.editClasses.SmallAuthor;
 import ru.mendeleev.api.entity.Country;
 import ru.mendeleev.api.entity.Genre;
 import ru.mendeleev.api.servcie.LibraryServerService;
+import ru.mendeleev.server.dao.interfaces.IAuthorDao;
+import ru.mendeleev.server.dao.interfaces.IBookDao;
+import ru.mendeleev.server.dao.interfaces.ICountryDao;
+import ru.mendeleev.server.dao.interfaces.IGenreDao;
+import ru.mendeleev.server.service.AuthManager;
 
-import java.util.Collections;
 import java.util.List;
 
 @Component
-public class LibraryServerServiceImpl implements LibraryServerService { // TODO !!!
+public class LibraryServerServiceImpl implements LibraryServerService {
+
+    private final IAuthorDao authorDao;
+    private final IBookDao bookDao;
+    private final ICountryDao countryDao;
+    private final IGenreDao genreDao;
+    private final AuthManager authManager;
+
+    public LibraryServerServiceImpl(IAuthorDao authorDao, IBookDao bookDao, ICountryDao countryDao, IGenreDao genreDao, AuthManager authManager) {
+        this.authorDao = authorDao;
+        this.bookDao = bookDao;
+        this.countryDao = countryDao;
+        this.genreDao = genreDao;
+        this.authManager = authManager;
+    }
 
     @Override
     public boolean isLoggedIn() {
-        return false;
+        return authManager.isLoggedIn();
     }
 
     @Override
     public List<FullAuthor> loadAllAuthors(AuthorFilter filter) {
-        return Collections.emptyList();
+        return authorDao.findAll(filter);
     }
 
     @Override
     public List<Country> loadAllCountries() {
-        return Collections.emptyList();
+        return countryDao.findAll();
     }
 
     @Override
     public List<FullBook> loadAllBooks() {
-        return Collections.emptyList();
+        return bookDao.findAll();
     }
 
     @Override
     public void saveAuthor(AuthorEdit authorEdit) {
-
+        authorDao.saveAuthor(authorEdit);
     }
 
     @Override
     public List<FullBook> loadAuthorBooks(Integer authorId) {
-        return Collections.emptyList();
+        return bookDao.findAuthorBooks(authorId);
     }
 
     @Override
     public List<FullBook> loadNotAllBooks(Integer authorId) {
-        return Collections.emptyList();
+        return bookDao.findNotAllBooks(authorId);
     }
 
     @Override
     public void updateAuthor(Integer authorId, AuthorEdit changedAuthor) {
-
+        authorDao.update(authorId, changedAuthor);
     }
 
     @Override
     public void deleteAuthorById(Integer authorId) {
-
+        authorDao.deleteAuthorById(authorId);
     }
 
     @Override
     public void deleteAuthorBooks(Integer authorId) {
-
+        bookDao.deleteAuthorBooks(authorId);
     }
 
     @Override
     public List<SmallAuthor> loadSmallAuthors() {
-        return Collections.emptyList();
+        return authorDao.findSmallAuthors();
     }
 
     @Override
     public List<Genre> loadAllGenres() {
-        return Collections.emptyList();
+        return genreDao.findAll();
     }
 
     @Override
     public List<FullBook> loadAllBooks(BookFilter bookFilter) {
-        return Collections.emptyList();
+        return bookDao.findAll(bookFilter);
     }
 
     @Override
     public void saveBook(BookEdit bookEdit) {
-
+        bookDao.saveBook(bookEdit);
     }
 
     @Override
     public void updateBook(Integer bookId, BookEdit changedBook) {
-
+        bookDao.update(bookId, changedBook);
     }
 
     @Override
     public void deleteBookById(Integer bookId) {
-
+        bookDao.deleteBookById(bookId);
     }
 
     @Override
     public boolean login(String login, String password) {
-        return false;
+        return authManager.login(login, password);
     }
 
     @Override
     public void logout() {
-
+        authManager.logout();
     }
 }
--- library-server/src/main/java/ru/mendeleev/server/service/AuthManager.java
+++ library-server/src/main/java/ru/mendeleev/server/service/AuthManager.java
@@ -1,12 +1,23 @@
 package ru.mendeleev.server.service;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Setter
 @Component
 public class AuthManager {
+
+    private static final String LOGIN = "1";
+    private static final String PASSWORD = "1";
+
     private boolean loggedIn = false;
+
+    public boolean login(String login, String password) {
+        loggedIn = LOGIN.equals(login) && PASSWORD.equals(password);
+        return loggedIn;
+    }
+
+    public void logout() {
+        loggedIn = false;
+    }
 }
